4dcb21b21260326cbeaa95826564c8959d297b4c
Natã <natanpedrodasilva@gmail.com>
2024-08-09 19:22:08 -0300

    Initial commit: Set up project structure

---
+++ dietsoft/dietsoft-backend/src/main/java/com/example/dietsoft/dietfood_backend/DietsoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.dietsoft.dietfood_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietsoftApplication.class, args);
+	}
+
+}
---
+++ dietsoft/dietsoft-backend/src/test/java/com/example/dietsoft/dietfood_backend/DietsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dietsoft.dietfood_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
