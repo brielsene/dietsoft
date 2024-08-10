8d01b62e6d85fa21e45cf908aab827ae5db4abb7
Natã <natanpedrodasilva@gmail.com>
2024-08-10 18:54:19 -0300

    config: Configuring H2

--- dietsoft/dietsoft-backend/src/main/java/com/example/dietsoft/dietfood_backend/entities/User.java
+++ dietsoft/dietsoft-backend/src/main/java/com/example/dietsoft/dietfood_backend/entities/User.java
@@ -1,40 +1,65 @@
-package entities;
+package com.example.dietsoft.dietfood_backend.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-@Table
 @Entity
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "TB_USERS")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "uuid2")
+    @JsonIgnore
+    private UUID id;
 
+    @Setter
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
 
+    @Setter
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
     private String email;
 
-
+    @Setter
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    @JsonIgnore
     private String password;
 
+
+    @Setter
     @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime createdAt;
 
+    @Setter
     @Column(name = "updated_at")
+    @UpdateTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime updatedAt;
 
+    @Setter
     @Column(name = "password_last_updated")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime passwordLastUpdated;
-
-
 }
