43a15eb769aea782f5c4cc23540938f31d367f8a
Gabriel Sene <sene300@gmail.com>
2024-08-12 23:34:26 -0300

    feat: Add a Enum CategoriaAlimento

---
+++ dietsoft/dietsoft-backend/src/main/java/com/example/dietsoft/dietfood_backend/entities/CategoriaAlimentos.java
@@ -0,0 +1,21 @@
+package com.example.dietsoft.dietfood_backend.entities;
+
+public enum CategoriaAlimentos {
+    CARBOIDRATOS,
+    PROTEINAS,
+    GORDURAS,
+    LATICINIOS,
+    VEGETAIS,
+    FRUTAS,
+    LEGUMINOSAS,
+    OLEAGINOSAS,
+    DOCES_E_SOBREMESAS,
+    BEBIDAS,
+    GRAOS_E_CEREAIS,
+    PROCESSADOS_E_INDUSTRIALIZADOS,
+    SUPLEMENTOS,
+    CONDIMENTOS_E_TEMPEROS,
+    SNACKS_E_PETISCOS;
+
+
+}
