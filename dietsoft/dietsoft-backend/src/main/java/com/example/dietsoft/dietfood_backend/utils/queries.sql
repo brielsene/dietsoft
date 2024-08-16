0a386f3bed055a288d766c8e05e5bc1f6f73a7d7
Gabriel Sene <sene300@gmail.com>
2024-08-16 00:08:57 -0300

    feat: add util insert to use when create database h2

--- dietsoft/dietsoft-backend/src/main/java/com/example/dietsoft/dietfood_backend/utils/queries.sql
+++ dietsoft/dietsoft-backend/src/main/java/com/example/dietsoft/dietfood_backend/utils/queries.sql
@@ -1,3 +1,4 @@
+--JSON INSERT
 [
     {
         "name": "Arroz Integral",
@@ -104,3 +105,16 @@
         "medidaCaseira": "1/4 xícara"
     }
 ]
+
+--SQL INSERT
+INSERT INTO tb_alimentos (name, qtd_proteina, qtd_carbo, caloria, qtd_gordura, qtd_fibra, qtd_sodio, qtd_acucares, categoria, porcao, medida_caseira)
+VALUES
+('Arroz Integral', '2.6g', '23g', '110', 0.9, 1.8, 0.0, 0.0, 'CARBOIDRATOS', 50.0, '1/4 xícara'),
+('Peito de Frango', '31g', '0g', '165', 3.6, 0.0, 74.0, 0.0, 'PROTEINAS', 100.0, '1 filé médio'),
+('Abacate', '2g', '9g', '160', 15.0, 7.0, 7.0, 0.7, 'GORDURAS', 100.0, '1/2 unidade'),
+('Leite Desnatado', '8g', '12g', '90', 0.2, 0.0, 120.0, 12.0, 'LATICINIOS', 200.0, '1 copo'),
+('Brócolis', '3g', '7g', '55', 0.6, 2.6, 33.0, 1.5, 'VEGETAIS', 100.0, '1 xícara'),
+('Banana', '1.3g', '27g', '105', 0.3, 3.1, 1.0, 14.0, 'FRUTAS', 120.0, '1 unidade média'),
+('Feijão Preto', '9g', '23g', '132', 0.5, 7.5, 1.0, 0.3, 'LEGUMINOSAS', 100.0, '1/2 xícara'),
+('Nozes', '4.3g', '14g', '185', 18.5, 2.0, 1.0, 1.0, 'OLEAGINOSAS', 28.0, '1/4 xícara');
+
